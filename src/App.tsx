3914bd50e358d3e5143d728aeddb351c8a47ad06
Gustav <sir.gustav.the.coder@gmail.com>
2021-04-02 23:53:42 +0200

    started implementing the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Container, Row, Button, Col, Navbar, Nav, ButtonGroup, Form } from 'react-bootstrap';
+
+import * as game from './game'
+
+const MonsterFight = (props: { monster: game.Monster }) => {
+    return (
+        <>
+
+            <Form>
+                <Form.Group as={Row}>
+                    <Form.Label column sm="2">
+                        Monster
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="text" placeholder={props.monster.name} readOnly />
+                    </Col>
+
+                    <Form.Label column sm="2">
+                        HP
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="text" placeholder={props.monster.hp.current.toString()} readOnly />
+                    </Col>
+                </Form.Group>
+            </Form>
+            <ButtonGroup aria-label="Actions">
+                <Button variant="secondary">Shout</Button>
+                <Button variant="secondary">Attack</Button>
+                <Button variant="secondary">Magic</Button>
+            </ButtonGroup>
+        </>
+    );
+};
+
+const Game = (props: {state: game.State}) => {
+    if(props.state.monster != null)
+    {
+        // fight monsters
+        return <MonsterFight monster={props.state.monster}/>;
+    }
+    else
+    {
+        // buy things in the city
+        return <></>;
+    }
+}
+
+function App()
+{
+    const [state, setState] = React.useState(game.new_game()); 
+    const newGame = () =>
+    {
+        setState(game.new_game());
+    };
+    return (
+        <>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">XFx</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home" onClick={newGame}>New game</Nav.Link>
+                </Nav>
+            </Navbar>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col xs lg="2" />
+                        <Col md="auto">
+                            <Game state={state}/>
+                        </Col>
+                    <Col xs lg="2" />
+                </Row>
+            </Container>
+        </>
+    );
 }
 
 export default App;
